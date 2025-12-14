d7b446f4d9df2359f5b96d7ac7433969098b4944
YYuzurihara <youtairangyuan@gmail.com>
2025-12-14 20:04:20 +0900

    #8 正解率の処理を修正

--- analyze_baseline.py
+++ analyze_baseline.py
@@ -24,17 +24,24 @@ text = get_answer()
 target_tokens = model.to_tokens(text)[0, POS_TO_START_SOLVE:-1] # (n)
 
 # baselineのlogitsから, lossを計算する
-loss = F.cross_entropy(baseline_logits[0,:,:], target_tokens, reduction="none")
+loss = F.cross_entropy(baseline_logits[0,:,:], target_tokens, reduction="none") # (n)
 
 probs = F.softmax(baseline_logits, dim=-1) # (1, n, vocab_size)
-correct_probs = probs[0, torch.arange(probs.shape[1]), target_tokens[0]] # (n)
+correct_probs = probs[0, torch.arange(probs.shape[1]), target_tokens] # (n)
 geom_mean = torch.exp(torch.mean(torch.log(correct_probs)))
+
+losses = []
+for i in range(loss.shape[0]):
+    item = {
+        "target": model.to_string(target_tokens[i].item()),
+        "loss": loss[i].item(),
+        "accuracy": correct_probs[i].item(),
+    }
+    losses.append(item)
 
 os.makedirs("results", exist_ok=True)
 with open("results/results_baseline.json", "w", encoding="utf-8") as f:
     json.dump({
         "accuracy": geom_mean.item(),
-        "losses": {
-            f"position_{i}": loss[i].item() for i in range(loss.shape[0])
-        }
-    }, f, indent=2, ensure_ascii=False)
+        "losses": losses
+    }, f, indent=4, ensure_ascii=False)
--- tokenize_test.py
+++ tokenize_test.py
@@ -1,14 +1,18 @@
 from transformer_lens import HookedTransformer
-from prompt_hanoi import get_answer, PROMPT_HANOI
+from prompt_hanoi import get_answer, POS_TO_START_SOLVE
 
 model = HookedTransformer.from_pretrained("meta-llama/Llama-3.1-8B", device="cpu")
 
 text = get_answer()
 tokens = model.to_str_tokens(text, prepend_bos=True)
 print(f"length of tokens: {len(tokens)}")
-print(f"Tokens: {tokens[331:]}")
+print(f"Tokens: {tokens[POS_TO_START_SOLVE:]}")
 
-print(''.join(tokens[:332]))
+print("prompt:")
+print(''.join(tokens[:POS_TO_START_SOLVE]), end="\n\n")
+
+print("target:")
+print(''.join(tokens[POS_TO_START_SOLVE:]), end="\n\n")
 
 # # token_ids = model.to_tokens(text, prepend_bos=True).tolist()
 # print(f"length of token_ids: {len(token_ids[0])}")
