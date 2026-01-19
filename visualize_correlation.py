a5849ca34e047245b6540150ef83a003750ad499
YYuzurihara <youtairangyuan@gmail.com>
2026-01-19 19:04:52 +0900

    #25 活性分布もプロット

--- visualize_correlation.py
+++ visualize_correlation.py
@@ -35,10 +35,11 @@ def find_max_correlation(
 
 def visualize_correlation(
     save_path: str,
-    base: str = "act_id1",
-    mode: str = "mean", # mean
+    base: str = "act_id1"
 ) -> None:
     assert base in ["act_id1", "act_id2"], "base must be 'act_id1' or 'act_id2'"
+
+    labels = ["Llama-3.1-8B", "DeepSeek-R1-Distill-Llama-8B"]
 
     heatmap = np.zeros((32, 32), dtype=float)
     for layer1 in tqdm(range(32)):
@@ -50,19 +51,61 @@ def visualize_correlation(
     plt.figure(figsize=(10, 8))
     plt.imshow(heatmap, origin="lower", aspect="auto", cmap="viridis")
     plt.colorbar(label="max correlation")
-    plt.xlabel("DeepSeek-R1-Distill-Llama-8B")
-    plt.ylabel("Llama-3.1-8B")
-    plt.title(f"Feature correlation ({mode}, base={base})")
+    if base == "act_id1":
+        plt.xlabel(labels[1])
+        plt.ylabel(labels[0])
+    else:
+        plt.xlabel(labels[0])
+        plt.ylabel(labels[1])
+    plt.title(f"Feature correlation")
     plt.tight_layout()
     plt.savefig(save_path)
     plt.close()
 
+def visualize_correlation_dist(
+    save_path: lambda layer: str,
+    base: str = "act_id1",
+    debug: bool = False
+) -> None:
+    """
+    同じ層の活性化値の相関の分布をプロットする
+    """
+    for layer in tqdm(range(32)):
+        if debug:
+            max_corrs = find_max_correlation(layer, layer, base=base)
+            print(f"max correlation of layer {layer}: {max_corrs.mean()}")
+            top_100 = max_corrs.sort_values(ascending=False).head(100)
+            print(top_100.values)
+        else:
+            max_corrs = find_max_correlation(layer, layer, base=base)
+            plt.hist(max_corrs, bins=10)
+            plt.title(f"Feature correlation distribution (layer={layer})")
+            plt.xlabel("max correlation")
+            plt.ylabel("frequency")
+            plt.savefig(save_path(layer))
+            plt.close()
+
 if __name__ == "__main__":
-    visualize_correlation(
-        save_path="images/correlation/correlation_heatmap_id1.png",
-        base="act_id1"
-    )
-    visualize_correlation(
-        save_path="images/correlation/correlation_heatmap_id2.png",
-        base="act_id2"
+    # visualize_correlation(
+    #     save_path="images/correlation/correlation_heatmap_id1.png",
+    #     base="act_id1"
+    # )
+    # visualize_correlation(
+    #     save_path="images/correlation/correlation_heatmap_id2.png",
+    #     base="act_id2"
+    # )
+
+    # visualize_correlation_dist(
+    #     save_path=lambda layer: f"images/correlation/correlation_distribution_id1_layer_{layer}.png",
+    #     base="act_id1"
+    # )
+    # visualize_correlation_dist(
+    #     save_path=lambda layer: f"images/correlation/correlation_distribution_id2_layer_{layer}.png",
+    #     base="act_id2"
+    # )
+
+    visualize_correlation_dist(
+        save_path=None,
+        base="act_id1",
+        debug=True
     )
